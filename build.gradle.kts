e2ec106cf6930758510d72a195bbaa6a85428825
Arxing <meiswind711108@gmail.com>
2023-07-04 00:48:51 +0800

    Define the gradle tasks "cleanDemo", "deployDemo" and "runDemo"

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,3 +9,22 @@ plugins {
   alias(libs.plugins.kotlin.jvm) apply false
   alias(libs.plugins.shadow) apply false
 }
+
+tasks.register("deployDemo") {
+  group = "xlack"
+  dependsOn(":xlack-runner:deployDemoRunner")
+  dependsOn(":xlack-demo-plugin:deployDemoPlugin")
+}
+
+tasks.register<Delete>("cleanDemo") {
+  group = "xlack"
+  delete = setOf(rootProject.files("demo"))
+}
+
+tasks.register<JavaExec>("runDemo") {
+  group = "xlack"
+  dependsOn(":deployDemo")
+  mainClass.set("org.arxing.xlack.MainKt")
+  args = listOf("--config", "xlack-config.json", "start")
+  classpath = rootProject.files("demo/xlack-runner.jar")
+}
