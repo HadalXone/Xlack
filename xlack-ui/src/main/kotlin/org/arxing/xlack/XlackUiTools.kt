13f70f01985a1e8ea08e24b6ccafaa3a60e9ec4d
Arxing <meiswind711108@gmail.com>
2023-07-04 00:32:41 +0800

    Add XlackInteractive

---
+++ xlack-ui/src/main/kotlin/org/arxing/xlack/XlackUiTools.kt
@@ -0,0 +1,13 @@
+package org.arxing.xlack
+
+import com.slack.api.Slack
+import org.koin.core.component.KoinComponent
+import org.koin.core.component.inject
+
+object XlackUiTools : KoinComponent {
+  private val config: XlackConfig by inject()
+
+  val slack: Slack by inject()
+
+  val botUserToken: String get() = config.botUserToken
+}
---
+++ xlack-ui/src/main/kotlin/org/arxing/xlack/interactive/XlackMessageInteractive.kt
@@ -0,0 +1,132 @@
+package org.arxing.xlack.interactive
+
+import com.slack.api.Slack
+import com.slack.api.methods.response.chat.ChatDeleteResponse
+import com.slack.api.methods.response.chat.ChatPostEphemeralResponse
+import com.slack.api.methods.response.chat.ChatPostMessageResponse
+import com.slack.api.methods.response.chat.ChatUpdateResponse
+import com.slack.api.model.Message
+import org.arxing.xlack.XlackUiTools
+import org.arxing.xlack.message.XlackMessage
+import org.arxing.xlack.message.xlackMessage
+
+interface XlackMessageInteractive {
+  val slack: Slack get() = XlackUiTools.slack
+  val defaultToken: String get() = XlackUiTools.botUserToken
+
+  fun XlackMessage.post(
+    channel: String,
+    token: String = defaultToken,
+    metadata: Message.Metadata? = null,
+    replyBroadcast: Boolean = false,
+    threadTs: String? = null,
+  ): ChatPostMessageResponse {
+    return slack.methods().chatPostMessage {
+      it.token(token)
+        .channel(channel)
+        .metadata(metadata)
+        .mrkdwn(enableMarkdown)
+        .replyBroadcast(replyBroadcast)
+        .threadTs(threadTs)
+        .attachments(attachments)
+        .blocks(blocks)
+        .text(text)
+    }
+  }
+
+  fun XlackMessage.postEphemeral(
+    channel: String,
+    userId: String,
+    token: String = defaultToken,
+    threadTs: String? = null,
+  ): ChatPostEphemeralResponse {
+    return slack.methods().chatPostEphemeral {
+      it.token(token)
+        .channel(channel)
+        .text(text)
+        .user(userId)
+        .attachments(attachments)
+        .blocks(blocks)
+        .threadTs(threadTs)
+    }
+  }
+
+  fun XlackMessage.postDirectMessage(
+    userId: String,
+    token: String = defaultToken,
+  ): ChatPostMessageResponse {
+    val conversation = slack.methods().conversationsOpen {
+      it.token(token).users(listOf(userId))
+    }
+    return post(channel = conversation.channel.id, token = token)
+  }
+
+  fun deleteMessage(
+    channel: String,
+    messageTs: String,
+    token: String = defaultToken,
+  ): ChatDeleteResponse {
+    return slack.methods().chatDelete {
+      it.token(token)
+        .channel(channel)
+        .ts(messageTs)
+    }
+  }
+
+  fun XlackMessage.update(
+    channel: String,
+    messageTs: String,
+    token: String = defaultToken,
+  ): ChatUpdateResponse {
+    return slack.methods().chatUpdate {
+      it.token(token)
+        .channel(channel)
+        .ts(messageTs)
+        .text(text)
+        .blocks(blocks)
+        .attachments(attachments)
+    }
+  }
+
+  fun getMessage(
+    channel: String,
+    messageTs: String,
+    token: String = defaultToken,
+  ): Message? {
+    return slack.methods().conversationsHistory {
+      it.token(token)
+        .channel(channel)
+        .latest(messageTs)
+        .inclusive(true)
+        .limit(1)
+    }
+      .messages
+      ?.singleOrNull()
+  }
+
+  fun findMessages(
+    channel: String,
+    token: String = defaultToken,
+    filter: (Message) -> Boolean,
+  ): List<Message> {
+    return slack.methods().conversationsHistory {
+      it.token(token)
+        .channel(channel)
+    }
+      .messages
+      ?.filter(filter)
+      .orEmpty()
+  }
+
+  fun String.toPlaintextMessage(): XlackMessage {
+    return xlackMessage {
+      plaintext(this@toPlaintextMessage)
+    }
+  }
+
+  fun String.toMarkdownMessage(): XlackMessage {
+    return xlackMessage {
+      markdown(this@toMarkdownMessage)
+    }
+  }
+}
